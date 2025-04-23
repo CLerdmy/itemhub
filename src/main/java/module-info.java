b2b3f8df63cc0a497f572a0cc46ae364bdaf6453
CLerdmy <podturov@gmail.com>
2025-04-23 19:30:07 +0300

    update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,14 @@
 module dev.clerdmy.itemhub {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.hibernate.orm.core;
+    requires jakarta.persistence;
+    requires java.naming;
 
 
     opens dev.clerdmy.itemhub to javafx.fxml;
     exports dev.clerdmy.itemhub;
+
+    opens dev.clerdmy.itemhub.ui.controllers to javafx.fxml;
+    exports dev.clerdmy.itemhub.ui.controllers;
 }
