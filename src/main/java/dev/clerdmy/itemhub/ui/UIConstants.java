fec1f36dcda10e2fd67595a44e45a713e01a1c7a
CLerdmy <podturov@gmail.com>
2025-04-25 23:58:56 +0300

    const

---
+++ src/main/java/dev/clerdmy/itemhub/ui/UIConstants.java
@@ -0,0 +1,10 @@
+package dev.clerdmy.itemhub.ui;
+
+public class UIConstants {
+
+    public static final int DEFAULT_WIDTH = 600;
+    public static final int DEFAULT_HEIGHT = 600;
+    public static final int MIN_WIDTH = 600;
+    public static final int MIN_HEIGHT = 600;
+
+}
